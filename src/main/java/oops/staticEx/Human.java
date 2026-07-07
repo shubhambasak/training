4583055ad35b8d218553ab184c273286568cdae0
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-07 09:22:15 +0530

    few more

---
+++ src/main/java/oops/staticEx/Human.java
@@ -0,0 +1,13 @@
+package oops.staticEx;
+
+public class Human {
+    int age;
+    double height;
+    int weight;
+
+    public Human(int age, double height, int weight){
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/oops/staticEx/Main.java
@@ -0,0 +1,12 @@
+package oops.staticEx;
+
+public class Main {
+    public static void main(String[] args){
+        Human h1 = new Human(19,5.11,42);
+        Human h2 = new Human(20,5.8,63);
+
+        System.out.println(h1.age);
+        System.out.println(h2.age);
+    }
+}
+
