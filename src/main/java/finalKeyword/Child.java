322932f1a33e7a796d09c2271e313cab8115e35e
Shubham Basak <bloggershubham7011@gmail.com>
2026-07-13 09:45:33 +0530

    Contents: Few topics like final and static. Core oops and algorithms.

--- src/main/java/finalKeyword/Child.java
+++ src/main/java/finalKeyword/Child.java
@@ -1,13 +1,13 @@
 package finalKeyword;
 
 public class Child extends Parent{
-    @Override
-    void fun1() {
-        System.out.println("Child fun1");
-    }
-
-    @Override
-    void fun12() {
-        System.out.println("Child fun2");
-    }
+//    @Override
+//    void fun1() {
+//        System.out.println("Child fun1");
+//    }
+//
+//    @Override
+//    void fun12() {
+//        System.out.println("Child fun2");
+//    }
 }
